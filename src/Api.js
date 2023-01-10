524d6549c72833f8f733b566b39886a826bf8474
Roshan <nkroshankumar@gmail.com>
2023-01-10 19:46:29 +0530

    new api added and small changes

--- src/Api.js
+++ src/Api.js
@@ -1,25 +1,37 @@
-
-const fetchAllElements = async(setIsLoading,filter='',search='',element='',setError)=>{
-
-    try{
+const fetchAllElements = async (
+    setIsLoading,
+    filter = "",
+    search = "",
+    setError
+) => {
+    try {
         setIsLoading(true)
         let res
-        if(filter=='')res = await fetch(`https://periodic-table-elements-info.herokuapp.com/elements`)
-        else res = await fetch(`https://periodic-table-elements-info.herokuapp.com/${element}/${filter}/${search}`)
-        const data = await res.json()
-        console.log(data,search)
+        if (filter == "")
+            res = await fetch(`https://neelpatel05.pythonanywhere.com`)
+        else
+            res = await fetch(
+                `https://neelpatel05.pythonanywhere.com/element/${filter}?${filter}=${search}`
+            )
+        let data = await res.json()
+
+        if (
+            filter == "atomicnumber" ||
+            filter == "atomicname" ||
+            filter == "symbol"
+        ) {
+            data = [data]
+        }
+
+        console.log(data, search)
         // setError(false)
 
         return data
-}catch(err){
-        if(filter=='')console.log('Error Has Occured')
-        else console.log(err,filter,search)
+    } catch (err) {
+        if (filter == "") console.log("Error Has Occured")
+        else console.log(err, filter, search)
         setError(true)
     }
-
-    
 }
-
-
 
 export default fetchAllElements
